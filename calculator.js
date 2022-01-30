8c18f26c90fd49b004bf14ad350f3954d3ba5a5e
Aguiyi <davidaguiyi9@gmail.com>
2022-01-30 13:41:57 +0100

    Handled key presses

--- calculator.js
+++ calculator.js
@@ -13,3 +13,42 @@ display.value = calculator.displayValue;
 }
 
 updateDisplay();
+
+const keys = document.querySelector('.calculator-keys');
+keys.addEventListener('click', (event) => {
+  // Access the clicked element
+  const { target } = event;
+
+  // Check if the clicked element is a button.
+  // If not, exit from the function
+  if (!target.matches('button')) {
+    return;
+  }
+
+  if (target.classList.contains('operator')) {
+    console.log('operator', target.value);
+    return;
+  }
+
+  if (target.classList.contains('percentage-key')) {
+    console.log('percentage-key', target.value);
+    return;
+  }
+
+  if (target.classList.contains('delete-key')) {
+    console.log('delete-key', target.value);
+    return;
+  }
+
+  if (target.classList.contains('decimal')) {
+    console.log('decimal', target.value);
+    return;
+  }
+
+  if (target.classList.contains('all-clear')) {
+    console.log('clear', target.value);
+    return;
+  }
+
+  console.log('digit', target.value);
+});
